ca5ff2569009e2a5659d47d0a874a56960cb9e1b
temssl <brayan324554@gmail.com>
2025-04-22 17:11:09 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import MeliNavbar from "./modules/NavBar";
 import Seccion3 from "./modules/Seccion3";
-import Modulo4 from "./modules/modulo4";
 
 function App() {
   return (
@@ -11,8 +10,6 @@ function App() {
 
       <div className="min-h-screen bg-gray-100 px-4 py-6">
         <div className="max-w-7xl mx-auto">
-          <Modulo4 />
-
           {/* Aquí puedes agregar más componentes o contenido de la página */}
           <div className="container mt-4">
             <h1 className="text-xl font-bold mb-2">Venta de productos</h1>
